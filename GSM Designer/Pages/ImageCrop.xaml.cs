0d5ed8e8a159184afcbea05fd5047b82a419da2e
Jay <jay.k@flipkart.com>
2016-05-16 16:03:25 +0530

    Cleanup

--- GSM Designer/Pages/ImageCrop.xaml.cs
+++ GSM Designer/Pages/ImageCrop.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace GSM_Designer
+namespace GSM_Designer.Pages
 {
     /// <summary>
     /// Interaction logic for ImageCrop.xaml
